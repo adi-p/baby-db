5d4e1b295fc0c114bc28883a8e44a11cbf16cb03
Adarvan Patel <Adarvan.p@hotmail.com>
2024-02-18 14:27:31 -0500

    Create basic FileStore functionality

--- FileStore.fs
+++ FileStore.fs
@@ -1,45 +1,95 @@
 
-
 module FileStore
+  open System
+  open System.IO
 
   // TODO::
-  // Do something similar to MemStore but actually write to a file. Base it on https://github.com/avinassh/py-caskdb/
+  // Do something similar to FileStore but actually write to a file. Base it on https://github.com/avinassh/py-caskdb/
+  // for writing to file look into streamwriter
 
-  // open System
-  // open System.IO
+  // FILE FORMATTING
 
-  // type Database = { 
-  //   Id: string;
-  //   IsPersistent: bool
-  // };
+  // IDEAS:
+  // Need to set the format for the KV pair and the header
+  // Cask_db does: 
+  // ┌───────────┬──────────┬────────────┬─────┬───────┐
+  // │ timestamp │ key_size │ value_size │ key │ value │
+  // └───────────┴──────────┴────────────┴─────┴───────┘
+  // ┌───────────────┬──────────────┬────────────────┐
+  // │ timestamp(4B) │ key_size(4B) │ value_size(4B) │
+  // └───────────────┴──────────────┴────────────────┘
+
+  let encodeKV timestamp (key: string) (value: string) =
+    // TODO: use timestamp!
+    let keyBytes = key |> System.Text.Encoding.ASCII.GetBytes
+    let valueBytes = value |> System.Text.Encoding.ASCII.GetBytes
+    let keySize = keyBytes |> Array.length |> BitConverter.GetBytes 
+    let valueSize = valueBytes |> Array.length |> BitConverter.GetBytes
+    let byteArray = seq { keySize; valueSize; keyBytes; valueBytes; } |> Array.concat
+    byteArray |> Array.length , byteArray
+  
+  let decodeKV (bytes: byte[]) =
+    // Get key size, get value size, read key, read value, return
+    // TODO: use timestamp
+    let keyStartIndex = 8; 
+    let keySize = bytes[0..3] |> BitConverter.ToInt32
+    let valueSize = bytes[4..7] |> BitConverter.ToInt32
+    let key = bytes[keyStartIndex..(keyStartIndex + keySize-1)] |> System.Text.Encoding.ASCII.GetString
+    let value = bytes[(keyStartIndex + keySize)..(keyStartIndex + keySize+valueSize-1)] |> System.Text.Encoding.ASCII.GetString
+    // printfn "Decoded Bytes %d %d %s %s " keySize valueSize key value
+    // printfn "buffer size %d" (bytes |> Array.length) 
+    
+    (0, key, value)
 
 
-  // let NOT_IMPLEMENTED = "Not implemented"
+  type FileStore = {
+    fileName : string
+    dataMap : Map<string, int*int> // a map from key to (byte length, byte offset in file)
+    writePosition: int
+  }
 
-  // // ** CREATE **
-  // let createDatabase databaseName =
-  //   { Id = databaseName; IsPersistent = false }
+  let empty fileName = 
+    // TODO: check if file already exists?
+    let file = File.Create fileName
+    file.Close ()
+    { dataMap = Map.empty; fileName = fileName; writePosition = 0 }
 
-  // // ** GET **
+  let load fileName =
+    empty fileName
+    // TODO: this should find the current file and create the dataMap and set the writePosition
 
-  // //TODO
-  // let getTable tableName = Error NOT_IMPLEMENTED
-
-  // //TODO
-  // let getItem tableName itemId = Error NOT_IMPLEMENTED
-
-  // //TODO
-  // let getItemByAttribute tableName attributeName attributeValue = Error NOT_IMPLEMENTED
-
-  // // ** PUT **
-
-  // //TODO
-  // let putTable tableName = Error NOT_IMPLEMENTED
-
-  // //TODO
-  // let putItem tableName item = Error NOT_IMPLEMENTED
+  
+  let set (fileStore: FileStore) (key: string) (value : string) =
+    // open file, encode KV pair, write to file, close file, update dataMap, update writePosition
+    let file = File.OpenWrite fileStore.fileName
+    let (bytesLength, encodedBytes) = encodeKV 0 key value
+    file.Seek (fileStore.writePosition,  SeekOrigin.Begin) |> ignore
+    file.Write encodedBytes
+    file.Close ()
+    { 
+      fileStore with 
+        dataMap = fileStore.dataMap |> Map.add key (bytesLength, fileStore.writePosition); 
+        writePosition = fileStore.writePosition + (encodedBytes |> Array.length);
+      }
 
 
-  // // ** DELETE **
 
-  // let deleteItem tableName itemId = Error NOT_IMPLEMENTED
+
+  let get (fileStore: FileStore) (key: string) =
+    // Find key in dataMap, open file, fetch data, close file, decode data, return
+    match Map.tryFind key fileStore.dataMap with
+    | None -> None
+    | Some (dataLength ,dataPosition) ->
+      let file = File.OpenRead fileStore.fileName
+      file.Seek (dataPosition,  SeekOrigin.Begin) |> ignore
+      let buffer = dataLength |> Array.zeroCreate<byte>
+      file.ReadExactly buffer
+      file.Close ()
+      // printfn "Encoded Bytes %A" (buffer |> System.Text.Encoding.ASCII.GetString)
+      let (_, _, value) = buffer |> decodeKV
+      Some value 
+    
+
+  let delete (fileStore: FileStore) (key: string) = ()
+    // { dataMap = Map.remove key fileStore.dataMap}
+    // TODO: idea -> remove from dataMap and add tombstone?
--- Program.fs
+++ Program.fs
@@ -1,6 +1,9 @@
 ﻿open System
 
-let test store =
+// This work is based on -> https://github.com/avinassh/py-caskdb/
+
+let testMemStore store =
+    printfn "Testing MemStore"
     let data =  [ ("a", "a"); ("b", "b"); ("c", "c"); ("d", "d"); ("e", "e"); ]
     let store = 
         data
@@ -8,12 +11,17 @@ let test store =
     let tests = [
         ("Should have 'a'", 
             fun (memStore : MemStore.MemStore) -> (MemStore.get memStore "a") = (Some "a"));
-        ("Should have 'b'",
+        ("Should have 'd'",
             fun (memStore : MemStore.MemStore) -> (MemStore.get memStore "d") = (Some "d")); 
         ("Should not have 'f'",
             fun (memStore : MemStore.MemStore) -> (MemStore.get memStore "f") = None); 
         ("Should still have 'a'",
             fun (memStore : MemStore.MemStore) -> (MemStore.get memStore "a") = (Some "a")); 
+        ("Should replace 'a'",
+            fun (memStore : MemStore.MemStore) ->
+                let memStore = (MemStore.set memStore "a" "a-replace") // TODO: change function contract so that chaining is easier
+                (MemStore.get memStore "a") = (Some "a-replace")
+        ); 
     ]
     let pass =
         tests
@@ -29,42 +37,61 @@ let test store =
     else 
         printfn "Not all tests pass"
 
+
+let testFileStore store =
+    printfn "Testing FileStore"
+    let data =  [ ("a", "a"); ("b", "bb"); ("c", "ccc"); ("d", "dd"); ("e", "e"); ]
+    let store = 
+        data
+        |> List.fold (fun acc (k, v) -> FileStore.set acc k v) store
+
+    // printfn "DB bytes"
+    // printfn "%A" ("testDB" |> System.IO.File.ReadAllBytes |> System.Text.Encoding.ASCII.GetString)
+    // printfn "%s" (FileStore.get store "a")
+    // printfn "%s" (FileStore.get store "b")
+    // printfn "%s" (FileStore.get store "c")
+    // printfn "%s" (FileStore.get store "d")
+    // printfn "%s" (FileStore.get store "e")
+
+    // TODO: write tests that do multiple things in a row rather than one thing per test
+
+    let tests = [
+        ("Should have 'a'", 
+            fun (fileStore : FileStore.FileStore) -> (FileStore.get fileStore "a") = (Some "a"));
+        ("Should have 'c'",
+            fun (fileStore : FileStore.FileStore) -> (FileStore.get fileStore "c") = (Some "ccc")); 
+        ("Should have 'd'",
+            fun (fileStore : FileStore.FileStore) -> (FileStore.get fileStore "d") = (Some "dd")); 
+        ("Should not have 'f'",
+            fun (fileStore : FileStore.FileStore) -> (FileStore.get fileStore "f") = None); 
+        ("Should still have 'a'",
+            fun (fileStore : FileStore.FileStore) -> (FileStore.get fileStore "a") = (Some "a"));
+        ("Should replace 'a'",
+            fun (fileStore : FileStore.FileStore) ->
+                let fileStore = (FileStore.set fileStore "a" "a-replace") // TODO: change function contract so that chaining is easier
+                (FileStore.get fileStore "a") = (Some "a-replace")
+        ); 
+    ]
+    let pass =
+        tests
+        |> List.fold 
+            (fun resultAcc (testName, testFunc) -> 
+                let result = testFunc store
+                printfn "%s - %b" testName result
+                result && resultAcc
+            )
+            true
+    if pass then
+        printfn "All tests pass"
+    else 
+        printfn "Not all tests pass"
+
+
 [<EntryPoint>]
 let main args = 
-    test MemStore.empty;
+    testMemStore MemStore.empty;
+    testFileStore (FileStore.empty "testDB")
     0
-
-//TODO:
-
-// Base work on -> https://github.com/avinassh/py-caskdb/
-// 
-
-
-// BELOW IS AN OLDER IDEA
-
-// create db
-// create/open folder 
-
-// create table
-// create/open file
-
-// - open db?
-
-// put item
-// new line? (*)
-
-// get item
-// scan files
-
-// put item 2 (edit?)
-
-// get item 2 (edit?)
-
-// delete item
-// scan and mark? (*) (are we doing this with delete flags?)
-
-// try get item (**) Do we nee this?
-//
 
 
 
