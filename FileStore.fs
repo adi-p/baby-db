120003f528021bab3f34bd0e8422e5451e633302
Adarvan Patel <Adarvan.p@hotmail.com>
2024-04-03 22:16:42 -0400

    Create OOP version of FileStore

--- FileStore.fs
+++ FileStore.fs
@@ -151,6 +151,77 @@ module FileStore
 
   let print (fileStore: FileStore) =
     fileStore
-    |> fold (fun () key value -> printfn "Key: %s Value: %s" key value; ()) ()
+    |> fold (fun _ key value -> printfn "Key: %s Value: %s" key value; ()) ()
     |> ignore
 
+  type FileStoreOO(fileName, load) =
+    let fileName = fileName
+
+    let mutable dataMap = Map.empty // TODO: assigning to data map seems weird
+
+    let mutable writeFileStream : FileStream = null
+
+    let mutable readFileStream : FileStream = null
+
+    let rec fillDataMap () =
+      let headerBuffer = HEADER_SIZE |> Array.zeroCreate<byte> // can this be moved above function def?
+      writeFileStream.ReadExactly headerBuffer
+      let keySize = headerBuffer[0..3] |> BitConverter.ToInt32
+      let valueSize = headerBuffer[4..7] |> BitConverter.ToInt32
+      let keyBuffer = keySize |> Array.zeroCreate<byte>
+      writeFileStream.ReadExactly keyBuffer
+      let key = keyBuffer |> bytesToString
+      let writeSize = (HEADER_SIZE + keySize + valueSize)
+
+      // remove if there is a tombstone
+      match valueSize with 
+      | 0 ->  dataMap <- dataMap |> Map.remove key;
+      | _ ->  dataMap <- dataMap |> Map.add key (writeSize, writeFileStream.Position); 
+
+      writeFileStream.Seek (valueSize, SeekOrigin.Current) |> ignore
+
+      match writeFileStream.Length <= writeFileStream.Position + int64(writeSize) with
+      | true -> () 
+      | false -> fillDataMap ()
+
+    let readFromFile (dataLength, dataPosition: int64) =
+        let buffer = dataLength |> Array.zeroCreate<byte>
+        readFileStream.Seek (dataPosition,  SeekOrigin.Begin) |> ignore
+        readFileStream.ReadExactly buffer
+        let (_, _, value) = buffer |> decodeKV
+        value
+
+    do 
+      if load then
+        writeFileStream <- File.Open (fileName, FileMode.Open, FileAccess.ReadWrite, FileShare.Read);
+        readFileStream <- File.Open (fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+      else
+        writeFileStream <- File.Open (fileName, FileMode.Create, FileAccess.ReadWrite, FileShare.Read)
+        readFileStream <- File.Open (fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+    
+    member this.Set (key: string) (value : string) =
+      // encode KV pair, write to file, update dataMap
+      let (bytesLength, encodedBytes) = encodeKV 0 key (value |> System.Text.Encoding.ASCII.GetBytes)
+      dataMap <- dataMap |> Map.add key (bytesLength, writeFileStream.Position);
+      writeFileStream.Write encodedBytes
+      // writeFileStream.Flush true; // TODO: will this be slow?
+    
+    member this.Get (key: string) =
+      writeFileStream.Flush true;
+
+      dataMap
+      |> Map.tryFind key
+      |> Option.map readFromFile
+
+    member this.Delete (key: string) = 
+      let (_, encodedBytes) = encodeKV 0 key [||]
+      writeFileStream.Write encodedBytes
+      // writeFileStream.Flush true;
+      dataMap <- dataMap |> Map.remove key
+
+    member this.Keys  = dataMap |> Map.keys
+
+    member this.Fold folder state = 
+      dataMap |> Map.fold (fun state key (dataLength, dataPosition: int64) -> readFromFile (dataLength, dataPosition) |> folder state key) state
+
+    member this.Print = this.Fold (fun _ key value -> printfn "Key: %s Value: %s" key value; ()) ()  |> ignore
--- Program.fs
+++ Program.fs
@@ -1,4 +1,4 @@
-open System
+﻿open System
 
 let testMemStore store =
     printfn "Testing MemStore"
@@ -39,14 +39,9 @@ let createFileStore store =
 
 let testFileStore store =
     printfn "Testing FileStore"
-    // let store = Option.defaultValue (createFileStore (FileStore.empty "testDB")) store 
-      
-
-    // printfn "DB bytes"
-    // printfn "%A" ("testDB" |> System.IO.File.ReadAllBytes |> System.Text.Encoding.ASCII.GetString)
+    // let store = Option.defaultValue (createFileStore (FileStore.empty "testDB")) store
 
     // TODO: write tests that do multiple things in a row rather than one thing per test
-
     let tests =
         [ ("Should have 'a'", (fun (fileStore: FileStore.FileStore) -> (fileStore |> FileStore.get "a") = (Some "a")))
           ("Should have 'c'", (fun (fileStore: FileStore.FileStore) -> (fileStore |> FileStore.get "c") = (Some "ccc")))
@@ -114,10 +109,51 @@ let testFileStoreLoad store =
     FileStore.close store; // this feels akward
 
 
+let createFileStoreOO (store: FileStore.FileStoreOO) =
+    let data = [ ("a", "a"); ("b", "bb"); ("c", "ccc"); ("d", "dd"); ("e", "e") ]
+    data |> List.fold (fun _ (k, v) -> (store.Set k v)) ()
+
+let testFileStoreOO (store: FileStore.FileStoreOO) =
+    printfn "Testing FileStoreOO"
+    let tests =
+        [ ("Should have 'a'", (fun (fileStore: FileStore.FileStoreOO) -> (fileStore.Get "a") = Some "a"))
+          ("Should have 'c'", (fun (fileStore: FileStore.FileStoreOO) -> (fileStore.Get "c") = (Some "ccc")))
+          ("Should have 'd'", (fun (fileStore: FileStore.FileStoreOO) -> (fileStore.Get "d") = (Some "dd")))
+          ("Should not have 'g'", (fun (fileStore: FileStore.FileStoreOO) -> (fileStore.Get "g") = None))
+          ("Should still have 'a'",
+           fun (fileStore: FileStore.FileStoreOO) -> (fileStore.Get "a") = Some "a")
+          ("Should replace 'a'",
+           fun (fileStore: FileStore.FileStoreOO) ->
+               (fileStore.Set "a" "a-replace" ; fileStore.Get "a") = (Some "a-replace"))
+          ("Should remove 'a'",
+           fun (fileStore: FileStore.FileStoreOO) -> (fileStore.Delete"a"; fileStore.Get "a") = None) 
+        ]
+
+    let pass =
+        tests
+        |> List.fold
+            (fun resultAcc (testName, testFunc) ->
+                let result = testFunc store
+                printfn "%s - %b" testName result
+                result && resultAcc)
+            true
+
+    if pass then
+        printfn "All tests pass"
+    else
+        printfn "Not all tests pass"
+
+
 // let largeTest n fileStore =
 //     (seq { for i in 1..n do yield  i.ToString() } : string seq)
 //     |> Seq.fold (fun store i -> FileStore.set i i store) fileStore
 //     |> FileStore.get (n.ToString())
+
+// let largeTestOO n (fileStore: FileStore.FileStoreOO) =
+//     (seq { for i in 1..n do yield  i.ToString() } : string seq)
+//     |> Seq.fold (fun store i -> fileStore.Set i i) ()
+    
+//     fileStore.Get (n.ToString())
 
 [<EntryPoint>]
 let main args =
@@ -125,7 +161,10 @@ let main args =
     testFileStore (createFileStore (FileStore.empty "testDB"))
     printfn "-- Load test --" // TODO: write tests for 'load' seperately
     testFileStoreLoad (FileStore.load "testDB")
-
+    
+    let ooStore = new FileStore.FileStoreOO("testDB", true)
+    createFileStoreOO(ooStore)
+    testFileStoreOO(ooStore)
     0
     // largeTest 100000 (FileStore.empty "testDB")
     // |> Option.defaultValue "wrong - Null"
